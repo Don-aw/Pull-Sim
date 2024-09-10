6922419a2cd9e6470c7262d3e65636490062ec94
Donaw <177387755+Don-aw@users.noreply.github.com>
2024-09-10 14:14:39 +0800

    Start rarity work

--- src/ui/RectTab.java
+++ src/ui/RectTab.java
@@ -9,6 +9,7 @@ public class RectTab {
     private int xPos, yPos, index, w = constant.BOX_WIDTH, h = constant.BOX_HEIGHT;
     private Random random = new Random();
     private Color color = new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256));
+    private int n, rarity;
 
     public void setXPos(int xPos) {
         this.xPos = xPos;
@@ -18,14 +19,24 @@ public class RectTab {
     }
 
     public RectTab () {
+        randomizeStar();
     }
 
     public void draw(Graphics g) {
         g.setColor(color);
         g.fillRect(xPos, yPos - h/2, w, h);
+        g.setColor(Color.black);
+        g.drawString(rarity + "", xPos, yPos - h/2);
     }
 
     public void update(Graphics g) {
         draw(g);
     }
+
+    public void randomizeStar() {
+        n = random.nextInt(100);
+        //rarity = n;
+        //add assign here
+    }
+
 }
